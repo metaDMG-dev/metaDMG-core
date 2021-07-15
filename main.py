d9cd9a9a381c289e70dd4a4fe0d7b61e4055ea27
Christian Michelsen <christianmichelsen@gmail.com>
2021-07-15 16:40:15 +0200

    Add better filtering

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pandas as pd
 from metaDMG.utils import load_config
 from metaDMG_fit import run_workflow
 from metaDMG_viz import start_dashboard
+from metaDMG.filters import load_results
 
 config = load_config()
 
-x=x
+df_results = load_results()
+
+x = x
 
 if __name__ == "__main__":
     run_workflow()
