a69ddd07bfd38c5bbef2c2dda2548e4031ee2437
Christian Michelsen <christianmichelsen@gmail.com>
2021-09-09 14:14:56 +0200

    test: Add the minimum amount of changes

---
+++ tests/test_metaDMG_cli.py
@@ -0,0 +1,21 @@
+from metaDMG import __version__
+from typer.testing import CliRunner
+from metaDMG.cli import cli_app
+
+
+def test_version():
+    assert __version__ >= "0.1.0"
+
+
+runner = CliRunner()
+
+
+def test_cli_app_version_command():
+    result = runner.invoke(cli_app, ["--version"])
+    assert result.exit_code == 0
+    assert "metaDMG CLI" in result.stdout
+
+
+def test_cli_app_version_command():
+    result = runner.invoke(cli_app, ["compute", "blabla"])
+    assert result.exit_code == 1
