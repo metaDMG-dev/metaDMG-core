c4585f78438cbb8935b53c500a66169ef1cf436f
Christian Michelsen <christianmichelsen@gmail.com>
2022-09-07 17:20:41 +0200

    fix: Improve speed of Bayesian confidence interval

--- src/metaDMG/fit/bayesian.py
+++ src/metaDMG/fit/bayesian.py
@@ -144,8 +144,8 @@ def compute_D_max(mcmc, data):
     alpha = Dx * phi
     beta = (1 - Dx) * phi
 
-    pdf = sp_betabinom(N, alpha, beta)
-    # pdf = sp_betabinom(N, alpha.mean(), beta.mean())  # 1000x faster approximation
+    # pdf = sp_betabinom(N, alpha, beta)
+    pdf = sp_betabinom(N, alpha.mean(), beta.mean())  # 1000x faster approximation
     return {
         "mu": mu.item(),
         "std": std.item(),
